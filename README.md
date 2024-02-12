2d8c31e7358a5ccc8ca78ee65886ba657d113d31
Otho <28bowersg@smtexas.org>
2024-02-12 13:27:54 -0600

    Added punctuation.py and updated README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # punctuation.py
 Keeps the punctuation of any .txt file
+## Usage
+1. Clone the repository with `git clone https://github.com/OthoXYZ/punctuation.py.git`
+2. cd to the directory `cd ~/punctuation.py/
+3. Change the input and output files to existing files in the directory
+4. Have fun!
+
+## Future Features
+- Add a graph of how often each punctuation mark occurs
+
+## Contributing
+If you have a way to optimize the code or you want to change something, just make a pull request.
+
+## License
+Licensed under the Unlicense.
---
+++ punctuation.py
@@ -0,0 +1,27 @@
+print('''                               __                      __                  
+                              /\\ \\__                  /\\ \\__  __           
+ _____   __  __    ___     ___\\ \\ ,_\\  __  __     __  \\ \\ ,_\\/\\_\\    ___     ___        _____   __  __
+/\\ '__`\\/\\ \\/\\ \\ /' _ `\\  /'___\\ \\ \\/ /\\ \\/\\ \\  /'__`\\ \\ \\ \\/\\/\\ \\  / __`\\ /' _ `\\     /\\ '__`\\/\\ \\/\\ \\
+\\ \\ \\L\\ \\ \\ \\_\\ \\/\\ \\/\\ \\/\\ \\__/\\ \\ \\_\\ \\ \\_\\ \\/\\ \\L\\.\\_\\ \\ \\_\\ \\ \\/\\ \\L\\ \\/\\ \\/\\ \\  __\\ \\ \\L\\ \\ \\ \\_\\ \\
+ \\ \\ ,__/\\ \\____/\\ \\_\\ \\_\\ \\____\\\\ \\__\\\\ \\____/\\ \\__/.\\_\\\\ \\__\\\\ \\_\\ \\____/\\ \\_\\ \\_\\/\\_\\\\ \\ ,__/\\/`____ \\
+  \\ \\ \\/  \\/___/  \\/_/\\/_/\\/____/ \\/__/ \\/___/  \\/__/\\/_/ \\/__/ \\/_/\\/___/  \\/_/\\/_/\\/_/ \\ \\ \\/  `/___/> \\
+   \\ \\_\\                                                                                  \\ \\_\\     /\\___/
+    \\/_/                                                                                    \\/_/     \\/__/''')
+
+import string
+
+def extract_punctuation(input_file, output_file):
+    with open(input_file, 'r') as file:
+        text = file.read()
+
+    punctuation = set(string.punctuation)
+    punctuation_in_text = ''.join(char for char in text if char in punctuation)
+
+    with open(output_file, 'w') as file:
+        file.write(punctuation_in_text)
+
+if __name__ == "__main__":
+    input_file = ("/path/to/input/file.txt")
+    output_file = ("/path/to/output/file.txt")
+    extract_punctuation(input_file, output_file)
+    print("Punctuation extracted successfully!")
